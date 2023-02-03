c12a760c6f2eb86a113899a9921ce3a2a79b1493
Álisson Henrique <alissonhmb@gmail.com>
2023-02-03 08:15:46 -0300

    Add category model and association to Task

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_30_125754) do
+ActiveRecord::Schema.define(version: 2023_02_03_105754) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -19,6 +25,7 @@ ActiveRecord::Schema.define(version: 2023_01_30_125754) do
     t.string "url"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
 end
